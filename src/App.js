2b088b81e94ae1ff1ce56832ae0269104504fba6
Irina <irinafawcett4@gmail.com>
2025-04-06 16:40:41 -0500

    initial site

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Card from './Card';
+
+function App() {
+  return (
+    <div className="App">
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Michroma&display=swap');
+      </style>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Michroma&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+      </style>
+      <header className="App-header">
+      <Card/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
