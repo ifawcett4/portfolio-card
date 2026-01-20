e363529949b6f3ff15c7873409f532a68bc52ff1
irina Fawcett <irinafawcett4@gmail.com>
2026-01-19 22:21:30 -0600

    Updates

--- src/components/testGLTF.js
+++
@@ -1,7 +0,0 @@
-// import { useLoader } from "@react-three/fiber";
-// import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-
-// function Scene() {
-//   const gltf = useLoader(GLTFLoader, "/project_assets/models/abstract.glb");
-//   return <primitive object={gltf.scene} />;
-// }
